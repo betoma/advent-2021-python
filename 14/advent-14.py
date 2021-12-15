c6a3502597c38c79587019c3cc5dcf072753de97
Bethany E. Toma <bethanyetoma@outlook.com>
2021-12-15 15:23:46 +0100

    rewrite day 14 solution to solve part 2

--- 14/advent-14.py
+++ 14/advent-14.py
@@ -1,36 +1,46 @@
-from collections import Counter
-from tqdm import tqdm
+from collections import Counter, defaultdict
 
-with open("test.txt") as f:
+with open("input.txt") as f:
     lines = f.readlines()
     template = lines[0].strip()
     rules = [tuple(x.strip().split(" -> ")) for x in lines[1:] if x != "\n"]
 
 insertion = {(x[0], x[1]): y for x, y in rules}
-pair_match = {x[0]: set() for x, _ in rules}
-for x, y in insertion:
-    pair_match[x].add(y)
 
 
-def naive_polymerization(template: str, steps: int):
-    polymer = template
-    yield polymer
-    for _ in range(steps):
-        final = len(polymer) - 1
-        new_polymer = []
-        for i, char in enumerate(polymer):
-            new_polymer.append(char)
-            if i < final and char in pair_match:
-                if (next := polymer[i + 1]) in pair_match[char]:
-                    new_polymer.append(insertion[(char, next)])
-        polymer = "".join(new_polymer)
-        yield polymer
+def polymerization(n: int):
+    letter_counts = Counter(template)
+    polymer = Counter([(char, template[i + 1]) for i, char in enumerate(template[:-1])])
+    yield polymer, letter_counts
+    for _ in range(n):
+        subtractions = Counter()
+        additions = Counter()
+        for pair in insertion:
+            if polymer[pair]:
+                subtractions[pair] += polymer[pair]
+                first_pair = (pair[0], insertion[pair])
+                additions[first_pair] += polymer[pair]
+                second_pair = (insertion[pair], pair[1])
+                additions[second_pair] += polymer[pair]
+                letter_counts[insertion[pair]] += polymer[pair]
+        polymer.update(additions)
+        polymer.subtract(subtractions)
+        yield polymer, letter_counts
 
 
 # part one
 
-for p in naive_polymerization(template, 10):
+for p, l in polymerization(10):
     pass
 
-ranks = Counter(p).most_common()
+ranks = l.most_common()
+print(ranks[0][1] - ranks[-1][1])
+
+
+# part two
+
+for p, l in polymerization(40):
+    pass
+
+ranks = l.most_common()
 print(ranks[0][1] - ranks[-1][1])
